ca71f55deefabc0656204747a7eaa6dcb9a07b8c
Craig Amundsen <cdamundsen@gmail.com>
2025-04-05 14:21:49 -0700

    Added Profile and supporting code

--- bird/merlin/views.py
+++ bird/merlin/views.py
@@ -2,7 +2,8 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
-from .forms import UserRegistrationForm
+from .forms import ProfileEditForm, UserEditForm, UserRegistrationForm
+from .models import Profile
 
 # Create your views here.
 def register(request):
@@ -15,6 +16,7 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             # Now save the User
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(
                 request,
                 'merlin/register_done.html',
@@ -40,5 +42,33 @@ def dashboard(request):
         'merlin/dashboard.html',
         {
             'section': 'dashboard',
+        }
+    )
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(
+            instance=request.user,
+            data=request.POST
+        )
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(
+        request,
+        'merlin/edit.html',
+        {
+            'user_form': user_form,
+            'profile_form': profile_form,
         }
     )
