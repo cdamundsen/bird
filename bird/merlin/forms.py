875567b6b3a0f74d26db75592288e1fc999d4cbc
Craig Amundsen <cdamundsen@gmail.com>
2025-04-04 20:08:00 -0700

    Added new account registration

--- bird/merlin/forms.py
+++ bird/merlin/forms.py
@@ -1,2 +1,24 @@
 from django import forms
+from django.contrib.auth import get_user_model
 
+
+class UserRegistrationForm(forms.ModelForm):
+    username = forms.CharField()
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label='Retype Password',
+        widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'first_name', 'email']
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords do not match.')
+        return cd['password2']
--- bird/merlin/views.py
+++ bird/merlin/views.py
@@ -2,8 +2,37 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import UserRegistrationForm
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new User instance, but don't save it to the database
+            new_user = user_form.save(commit=False)
+            # Let Django set the password
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Now save the User
+            new_user.save()
+            return render(
+                request,
+                'merlin/register_done.html',
+                {
+                    'new_user': new_user,
+                }
+            )
+    else:
+        user_form = UserRegistrationForm()
+    return render(
+        request,
+        'merlin/register.html',
+        {
+            'user_form': user_form,
+        }
+    )
+
+
 @login_required
 def dashboard(request):
     return render(
